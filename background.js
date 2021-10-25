9ec7f3830939ec4c054de84e8f50ca47484f0b39
AustinR3 <austinradhs@oakland.edu>
2021-10-25 12:30:05 -0400

    List to add blocked sites

--- background.js
+++ background.js
@@ -1,5 +1,20 @@
+const defaultFilters = [ //all the ads sites that will have their ad request blocked
+	"*://*.doubleclick.net/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
+]
+
+
+
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) { return { cancel: true }}, //blocks request that we are sending
-    { urls: ["*://*.zedo.com/*"] }, //urls that will be blocked (site thats providing the ads)
+    { urls: [defaultFilters] }, //urls that will be blocked (site thats providing the ads)
     ["blocking"]
 )
